6c24c6aa6d9eb72a483f888f853dceb47391e4d2
yaroslavvk <yaroslavvk@outlook.com>
2021-02-03 00:27:35 +0200

    added shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DumpComponent } from './dump/dump.component';
+
+
+
+@NgModule({
+  declarations: [DumpComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
